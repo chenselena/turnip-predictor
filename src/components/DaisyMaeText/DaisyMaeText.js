34a582fd5ee03d63e108285135729c71fb942a2a
Selena Chen <sleneachen@hotmail.com>
2021-01-06 16:35:43 -0800

    added bg and changed folder formats

---
+++ src/components/DaisyMaeText/DaisyMaeText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DaisyMaeImg from '../../images/daisy_mae.png';
+
+import './DaisyMaeText.css';
+
+function DaisyMaeText() {
+  return (
+    <div className='daisy-image'>
+      <img src={DaisyMaeImg} alt="DaisyMaeImg" />
+    </div>
+  );
+}
+
+export default DaisyMaeText;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DaisyMaeText from './DaisyMaeText/DaisyMaeText';
+import '../css/Main.css';
+
+function Main() {
+  return (
+    <div className="container">
+      <DaisyMaeText />
+    </div>
+  );
+}
+
+export default Main;
