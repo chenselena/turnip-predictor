604cabc198f584f1a27eef1788c2593458b809c9
chenselena <selenachenyz@gmail.com>
2021-01-31 15:23:31 -0800

    getting min and max values

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,3 +1,14 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
+
 import { Predictor } from "../../utils/predictor";
 function Chart() {
   let pat_desc = {
@@ -10,8 +21,23 @@ function Chart() {
   let prices = [99, 100, 86];
   let predict = new Predictor(prices, false, 0);
   let results = predict.analyze_possibilities();
+  for (let res of results) {
+    for (let day of res.prices.slice(2)) {
+      console.log("Day min: " + day.min);
+      console.log("Day max: " + day.max);
+    }
+    console.log("guaranteed min" + res.weekGuaranteedMinimum);
+    console.log("week max: " + res.weekMax);
+  }
 
-  return <div className="number-input-styles">{results[0]}</div>;
+  return (
+    <div className="number-input-styles">
+      {/* {results.map((res, i) => (
+        <Box>{res[i].prices.slice(2)}</Box>
+      ))} */}
+      hi
+    </div>
+  );
 }
 
 export default Chart;
